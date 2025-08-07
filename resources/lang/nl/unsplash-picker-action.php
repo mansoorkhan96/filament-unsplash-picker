80c234b93bfe1667291faa0d6813ff1d4920b314
Jelle Visser <j.visser@congos.nl>
2025-08-07 08:51:42 +0200

    Add Dutch translations

---
+++ resources/lang/nl/unsplash-picker-action.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'label' => 'Kies een afbeelding van Unsplash',
+    'description' => 'Je kunt 1 foto selecteren.|Je kunt :numberOfSelectableImages foto\'s selecteren.',
+    'form' => [
+        'fields' => [
+            'search' => [
+                'placeholder' => 'Afbeelding zoeken',
+            ],
+            'square_mode' => [
+                'label' => 'Vierkante modus',
+            ],
+        ],
+    ],
+    'actions' => [
+        'next_page' => [
+            'label' => 'Volgende',
+        ],
+        'previous_page' => [
+            'label' => 'Vorige',
+        ],
+    ],
+    'no_search_results' => "Sorry, je zoekopdracht heeft geen resultaten opgeleverd.<br>Probeer een andere zoekopdracht.",
+];
